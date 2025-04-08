6752f997d7085c8c3f57a8f84613f33fc258975f
Ryan Baas <rybaas293@gmail.com>
2025-04-07 22:04:46 -0400

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Give your session a name
 SESSION=mysession
 
 # Create a new tmux session
@@ -36,16 +37,15 @@ done
 # 1-8
 for i in {0..7}; do
   j=$(($i + 1))
-  tmux send-keys -t $SESSION:0.$i "echo gomp0${j}" Enter
+  tmux send-keys -t $SESSION:0.$i "fish" Enter "clear" Enter "echo gomp0${j}" Enter
 done
 
 # 9-16
 for i in {8..15}; do
   j=$(($i + 1))
   n=$(($j - 8))
-  tmux send-keys -t $SESSION:0.$i "echo comp0${n}" Enter
+  tmux send-keys -t $SESSION:0.$i "fish" Enter "clear" Enter "echo comp0${n}" Enter
 done
-
 
 # Attach to the session
 tmux attach-session -t $SESSION
